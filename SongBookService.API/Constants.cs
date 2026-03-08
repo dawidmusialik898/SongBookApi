30b53e7cf495dc337d983a7daea5d77cc0d7f86e
Dawid Musialik <dawidmusialik1998@gmail.com>
2026-03-08 19:35:52 +0100

    vipeout

--- SongBookService.API/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SongBookService.API
-{
-    internal static class Constants
-    {
-        internal static readonly string[] _newLineSymbols = { "\r\n", "\r", "\n" };
-    }
-}
--- SongBookService.API/Options/SongRepositoryOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SongBookService.API.Options
-{
-    public class SongRepositoryOptions
-    {
-        public string DatabaseName { get; set; } = string.Empty;
-        public string CollectionName { get; set; } = string.Empty;
-    }
-}
